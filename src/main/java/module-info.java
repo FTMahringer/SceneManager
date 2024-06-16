10582a8ee7ccc4d474aad5023e65c029f572cd9f
Fynn Mahringer <168755023+FTMahringer@users.noreply.github.com>
2024-06-16 22:43:43 +0200

    Delete src/main/java directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module at.fmahring.scenemanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens at.fmahring.scenemanager to javafx.fxml;
-    exports at.fmahring.scenemanager;
-}
