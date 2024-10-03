35b556d13fd69c1304b00ac73a049f975a6fcb84
Fynn Mahringer <168755023+FTMahringer@users.noreply.github.com>
2024-10-03 10:48:16 +0200

    Update SceneManager.java

--- SceneManager.java
+++ SceneManager.java
@@ -1,4 +1,6 @@
-import at.ftmahringer.frameworkfirst.utils.Scenes;
+import at.htl.snake.game.GameController;
+import at.htl.snake.utils.KeyHandler;
+import at.htl.snake.utils.Scenes;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,16 +18,16 @@ public class SceneManager {
     private final BorderPane rootLayout;
     private static final Deque<Parent> sceneStack = new ArrayDeque<>();
 
-    private static String PopUpStageTitle;
+    private static Stage popUpStage;
+    private static final Deque<Scenes> popUpSceneStack = new ArrayDeque<>();
 
     public SceneManager(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.centerOnScreen();
         rootLayout = new BorderPane();
     }
 
     public void setScene(Scenes fxmlPath) {
-        // test if the fxmlPath is valid
         if (fxmlPath == null) {
             return;
         }
@@ -35,6 +37,11 @@ public class SceneManager {
             primaryStage.setTitle(fxmlPath.getTitle());
             sceneStack.push(scene);
             rootLayout.setCenter(scene);
+
+            scene.setFocusTraversable(true);
+            scene.setOnKeyPressed(keyEvent -> {
+                KeyHandler.getInstance().keyPressed(keyEvent);
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,30 +57,12 @@ public class SceneManager {
     public void showScene() {
         Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 
     public Stage getPrimaryStage() {
         return primaryStage;
-    }
-
-    public void setTitle(String title) {
-        primaryStage.setTitle(title);
-    }
-
-    public void setSceneSize(double width, double height) {
-        primaryStage.setWidth(width);
-        primaryStage.setHeight(height);
-    }
-
-    public void setSceneMinSize(double minWidth, double minHeight) {
-        primaryStage.setMinWidth(minWidth);
-        primaryStage.setMinHeight(minHeight);
-    }
-
-    public void setSceneMaxSize(double maxWidth, double maxHeight) {
-        primaryStage.setMaxWidth(maxWidth);
-        primaryStage.setMaxHeight(maxHeight);
     }
 
     public void setSceneResizable(boolean resizable) {
@@ -84,21 +73,78 @@ public class SceneManager {
         primaryStage.close();
     }
 
-    public static void PopUpWindow(Scenes fxml, String title, Window parentWindow) throws IOException {
-        Stage stage = new Stage();
-        stage.setTitle(title);
-        PopUpStageTitle = title;
-        stage.initModality(Modality.APPLICATION_MODAL); // or Modality.WINDOW_MODAL
-        stage.initOwner(parentWindow);
-        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource(fxml.getPath()));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.centerOnScreen();
-        stage.showAndWait();
+    public static void showPopUp(Scenes fxml) throws IOException {
+        showPopUp(fxml, Starter.getPrimaryStage());
     }
 
-    public static String getPopUpStageTitle() {
-        return PopUpStageTitle;
+    public static void showPopUp(Scenes fxml, Window parentWindow) throws IOException {
+        if (popUpStage == null) {
+            popUpStage = new Stage();
+            popUpStage.initModality(Modality.APPLICATION_MODAL);
+            popUpStage.initOwner(parentWindow);
+        }
+
+        setPopUpScene(fxml);
+        popUpStage.centerOnScreen();
+        popUpStage.showAndWait();
+    }
+
+    public static void goBackPopUp() throws IOException {
+        if (popUpSceneStack.size() > 1) {
+            popUpStage.hide(); // Hide the current pop-up stage
+            popUpSceneStack.pop();
+            setPopUpScene(popUpSceneStack.peek());
+            popUpStage.show(); // Show the new pop-up stage
+        } else {
+            closePopUpWindow(Scenes.ESCMENU);
+        }
+    }
+
+    public static void closePopUpWindow(Scenes escmenu) {
+        if (popUpStage != null) {
+            popUpStage.close();
+            popUpStage = null;
+        }
+
+        if (escmenu.equals(Scenes.ESCMENU)) {
+            GameController.resume();
+        }
+    }
+
+    public static void setPopUpScene(Scenes fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource(fxml.getPath()));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root); // Erstellen Sie hier eine neue Scene
+        popUpStage.setTitle(fxml.getTitle());
+        popUpStage.setScene(scene);
+
+        root.setOnKeyPressed(keyEvent -> {
+            KeyHandler.getInstance().keyPressed(keyEvent);
+        });
+
+        // on close Request
+        popUpStage.setOnCloseRequest(e -> {
+            try {
+                // this if for checking wich scene is there as a popup
+                if (fxml != Scenes.ESCMENU) {
+                    goBackPopUp();
+                    e.consume();
+                } else {
+                    closePopUpWindow(Scenes.ESCMENU);
+                }
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        popUpSceneStack.push(fxml);
+    }
+
+    public static String getCurrentSceneTitle() {
+        return Starter.getPrimaryStage().getTitle();
+    }
+
+    public static Scenes getCurrentScene() {
+        return popUpSceneStack.peek();
     }
 }
